899847cdcecb7342a3de9cf2e69a41da97a9b545
Nastyakorotkaa <Nastyakorotkaa72@gmail.com>
2024-04-16 12:56:44 +0700

    Добавили комментарий в исходный код

--- context1.c
+++ context1.c
@@ -1,3 +1,4 @@
+//Подключение библиотек
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -5,8 +6,10 @@
 //Главная функция программы
 int main(int argc, char *argv[], char *envp[]) {
 	int result;
+	//Запуск программы cat с аргементом otchet16_04_2024.txt
 	result = execle("/bin/cat", "/bin/cat", "otchet16_04_2024.txt", 0, envp);
+	//printf() выполняется только в случае ошибки вызова execle()
 	printf("Ошибка при авполнении системного вызова execle()\n");
 	exit(-1);
-	return 0;
+	return 0;//Никогда не выполняется
 }

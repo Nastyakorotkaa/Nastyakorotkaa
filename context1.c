38280593209016901ad82c45cad1de889434828f
Nastyakorotkaa <Nastyakorotkaa72@gmail.com>
2024-04-16 13:25:02 +0700

    Добавленны изменение котекста для дочернего процесса

--- context1.c
+++ context1.c
@@ -1,15 +1,40 @@
-//Подключение библиотек
-#include <sys/types.h>
-#include <stdlib.h>
+// Подключаем библиотеки
+#include<sys/types.h>
+#include<stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 //Главная функция программы
 int main(int argc, char *argv[], char *envp[]) {
-	int result;
-	//Запуск программы cat с аргементом otchet16_04_2024.txt
-	result = execle("/bin/cat", "/bin/cat", "otchet16_04_2024.txt", 0, envp);
-	//printf() выполняется только в случае ошибки вызова execle()
-	printf("Ошибка при авполнении системного вызова execle()\n");
-	exit(-1);
-	return 0;//Никогда не выполняется
+        // Объявление переменной
+        int result1;
+        //Запуск программы cat с аргементом otchet16_04_2024.txt
+        //Объявление переменных
+        pid_t procid,parentid;//Индетефикаторы процессов
+        pid_t result;// Результат системного вызова fork()
+        int a = 0;// переменная для проверки работы процесса-родителя и процесса-ребенка
+        result = fork();
+        if(result < 0) {
+                printf("Ошибка при выполнении fork()/n");
+                exit(-1);
+        }
+        else if (result == 0) { 
+                a = a+1;
+                procid = getpid();//Получение идeтификатора текущегопроцесса
+                parentid = getppid();//Получение идентификатора родительского процесса
+                printf("Ид. процесса: %d, ид. родителя: %d, значение переменной a: %d/n", procid, parentid, a);
+                result = execle("/bin/cat", "/bin/cat", "otchet16_04_2024.txt", 0, envp);
+                
+
+
+        }
+        else {
+                a = a+555;
+                procid = getpid();//Получение идeтификатора текущегопроцесса
+                parentid = getppid();//Получение идентификатора родительского процесса
+                printf("Ид. процесса: %d, ид. родителя: %d, значение переменной a: %d/n", procid, parentid, a);
+	        printf("Ошибка при авполнении системного вызова execle()\n");
+		exit(-1);
+ 	}
+        return 0;//Никогда не выполняется
 }
+

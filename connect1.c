0c0b69086a3997e43eb7f4f1c3c5275ee15513a4
Nastyakorotkaa <Nastyakorotkaa72@gmail.com>
2024-04-23 15:34:21 +0700

    Добавили комментарии в программы работы с файлами

--- connect1.c
+++ connect1.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+//Подключение библиотеки для управления файлами
 #include <fcntl.h>
 int main() {
-	int fd;
-	size_t size;
-	char string[]="Group 3.120.2.21";
+	int fd;//Файловый дискриптор
+	size_t size;//Размер строки в байтах
+	char string[]="Group 3.120.2.21\n";
+//Открытие файла на запись, если файла не существует, то он будет создан с правами доступа 0666
 	if((fd = open("file1.txt", O_WRONLY|O_CREAT, 0666))<0) {
 		printf("Ошибка при открывании файла на запись\n");
 		exit(-1);
 
 	}
-	size = (fd, string, 17);
-	if (size  !=17) {
+//Чтение из файла в строку string
+	size = write(fd, string, 18);
+	if (size  != 18) {
 		printf("Не удалось записать всю строку в файл\n");
 		exit(-1);
 	}
-	if (close(fd)<0) {
+//Закрытие файла
+	if (close(fd) < 0) {
                 printf("Не удалось записать файл\n");
                 exit(-1);
         }
--- pipe.c
+++ pipe.c
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
+// Создаем массив для хранения файловых дискрипторов для чтения и для записи
 	int fd[2];
 	size_t size;
 	char string[]="Hello,world!";
@@ -12,23 +13,30 @@ int main(){
 	/*Если создать pipe не удалось,печатаем в этом сообщении и прекращаем работу*/
 		printf("Can\'t create pipe\n");
 		exit(-1);
+//Возврат признака аварийного закрытия программы (-1)
 	}
 	size=write(fd[1],string,14);
+//Проверка количества байт, записанных в строку с помощию системного вызова write()
 	if (size !=14){
 		printf("Can\'t write all string\n");
 		exit(-1);
 	}
 	size=read(fd[0],resstring,14);
+//Проверка количества байт, прочитанных из pipe() c  помощию read()
 	if(size < 0){
         	printf("Cant\'t read string\n");
         	exit(-1);
         }
 	printf("%s\n",resstring);
+//Закратие pipe для чтения
 	if (close(fd[0])<0){
 		printf("Cant\'t close input stream\n");
+		 exit(-1);
 	}
+//Закрытие pipe для записи
 	if (close(fd[1])<0){
         	printf("Cant\'t close output stream\n");
+		 exit(-1);
         }
 return 0;
 }
@@ -37,7 +45,3 @@ return 0;
 
 
 
-
-
-
-	

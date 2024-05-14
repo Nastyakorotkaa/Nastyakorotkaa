463951ad64cc316e15da23c1227fc0689ff8a6fc
Nastyakorotkaa <Nastyakorotkaa72@gmail.com>
2024-05-14 11:43:23 +0700

    Создана программа, работающая с threads'ами

---
+++ threads.c
@@ -0,0 +1,27 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+int a = 0; 
+void *mythread(void *dummy){
+	pthread_t mythid;
+	mythid = pthread_self();
+	a = a+1;
+	printf("Thread %ld, Результат вычесления a= %d\n", mythid, a);
+	return NULL;
+}
+int main () {
+	pthread_t thid, mythid;
+	int result;
+	result = pthread_create(&thid, (pthread_attr_t *)NULL, mythread, NULL);
+	if (result != 0) {
+		printf("Ошибка при создании нового thread\'a , возвращенное значение =%d\n", result);
+		exit(-1);
+	}
+	printf("Thread создан, thid = %ld\n,", thid);
+	mythid = pthread_self();
+	a = a+1;
+	printf("Thread %ld, Результат вычесленияa = %d\n", mythid, a);
+	pthread_join(thid, (void **)NULL);
+	return 0;
+
+}
